f07759f04c7a9fa9244037db2d35157d85c33b14
lollipopfly <lollipop.fly@gmail.com>
2016-04-09 14:21:29 +0300

    make products page

---
+++ wp-content/themes/oceanloverz/products.php
@@ -0,0 +1,11 @@
+<?
+/*
+    Template Name: Products
+*/
+    get_header();
+?>
+    <h2 class="page__title page__title-blue">Products</h2>
+
+    <div class="coming-soon">Coming Soon!</div>
+
+<? get_footer();?>
